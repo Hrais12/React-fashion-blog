cd9642e79335534762c0c300cdd036a17a54de9b
Houda Rais <houdarais@Houdas-Air.fios-router.home>
2024-05-01 18:29:05 -0400

    img

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -8,7 +8,7 @@ function Article() {
 
     <time datetime="2020-12-11" className='time'>11/12/20</time>
     <h2 className='title'>On the Street in Brooklyn</h2>
-    <img src="https://ps-react-curriculum.herokuapp.com/fashionblog/blog-image-1.jpg" alt="pic1" />
+    <img src="/blog-image-1.jpg" alt="pic1" />
     <p>Cray Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta iusto libero a id eum facilis repellendus aperiam dolorem recusandae vel accusantium veniam unde optio commodi incidunt, eaque molestias eos porro.</p>
     <a href="#"><div className='continue'>Continue...</div></a>
   </article>
@@ -19,7 +19,7 @@ function Article() {
 
 <time datetime="2020-11-11" className='time'>11/11/20</time>
 <h2 className='title'>Vintage in Vogue</h2>
-<img src="https://ps-react-curriculum.herokuapp.com/fashionblog/blog-image-2.jpg" alt="pic2" />
+<img src="/blog-image-2.jpg" alt="pic2" />
 <p>Selfies ipsum dolor sit amet consectetur adipisicing elit. Soluta iusto libero a id eum facilis repellendus aperiam dolorem recusandae vel accusantium veniam unde optio commodi incidunt, eaque molestias eos porro.</p>
 <a href="#"><div className='continue'>Continue..</div></a>
 </article>
