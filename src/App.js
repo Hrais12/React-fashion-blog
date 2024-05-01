589f0c9f4fe7432970f088ce8f5985de843c3345
Houda Rais <houdarais@Houdas-Air.fios-router.home>
2024-05-01 19:25:37 -0400

    map through articles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,22 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 
 function App() {
+
+  let allArticles =[
+    {
+      date: "2020-12-11",
+      title: "On the Street in Brooklyn",
+      image: "/blog-image-1.jpg",
+      content: "Cray Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta iusto libero a id eum facilis repellendus aperiam dolorem recusandae vel accusantium veniam unde optio commodi incidunt, eaque molestias eos porro."
+    },
+    {
+      date: "2020-11-11",
+      title: "Vintage in Vogue",
+      image: "/blog-image-2.jpg",
+      content: "Selfies ipsum dolor sit amet consectetur adipisicing elit. Soluta iusto libero a id eum facilis repellendus aperiam dolorem recusandae vel accusantium veniam unde optio commodi incidunt, eaque molestias eos porro."
+    }
+  ];
+
   return (
 
     <div className='mainContainer'>
@@ -14,7 +30,7 @@ function App() {
     <div className="App">
       <Header/>
       <Nav/>
-      <Article/>
+      <Article articles={allArticles}/>
       <Footer/>
     </div>
     </div>
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,30 +1,21 @@
 import React from 'react'
 
-function Article() {
+function Article({articles}) {
   return (
     <div>
-        <section>
-  <article>
+       <section>
+  
+           {articles.map((article) => (
+          <article >
+            <time datetime={article.date} className='time'>{article.date}</time>
+            <h2 className='title'>{article.title}</h2>
+            <img src={article.image} alt='pic' />
+            <p>{article.content}</p>
+            <a href="#"><div className='continue'>Continue...</div></a>
+          </article>
+         ))}
 
-    <time datetime="2020-12-11" className='time'>11/12/20</time>
-    <h2 className='title'>On the Street in Brooklyn</h2>
-    <img src="/blog-image-1.jpg" alt="pic1" />
-    <p>Cray Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta iusto libero a id eum facilis repellendus aperiam dolorem recusandae vel accusantium veniam unde optio commodi incidunt, eaque molestias eos porro.</p>
-    <a href="#"><div className='continue'>Continue...</div></a>
-  </article>
-
-
-
-  <article >
-
-<time datetime="2020-11-11" className='time'>11/11/20</time>
-<h2 className='title'>Vintage in Vogue</h2>
-<img src="/blog-image-2.jpg" alt="pic2" />
-<p>Selfies ipsum dolor sit amet consectetur adipisicing elit. Soluta iusto libero a id eum facilis repellendus aperiam dolorem recusandae vel accusantium veniam unde optio commodi incidunt, eaque molestias eos porro.</p>
-<a href="#"><div className='continue'>Continue..</div></a>
-</article>
-
-</section>
+        </section>
     </div>
   )
 }
